3b2d521942d3365f71699e638a10e2fabf2ec552
Amanda Chawinska <amanda.chawinska@spoko.pl>
2024-10-06 19:12:13 +0200

    component mainMenu

---
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -0,0 +1,17 @@
+import { GENDERS } from "../../constants/categories";
+import styles from "./MainMenu.module.css";
+import { NavLink } from "react-router-dom";
+
+export function MainMenu() {
+  return (
+    <ul className={styles.mainMenu}>
+      {GENDERS.map((category) => {
+        return (
+          <li key={category.path}>
+            <NavLink to={category.path}>{category.categoryName}</NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
