90f3bb5b9e9ad95aefb755b5c83068d5c94531fc
Amanda Chawinska <amanda.chawinska@spoko.pl>
2024-11-28 16:39:32 +0100

    component pagination

---
+++ front-end/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,19 @@
+import styles from "./Pagination.module.css";
+
+import { NavLink } from "react-router-dom";
+
+export function Pagination({ numberOfPages }) {
+  const pages = Array(numberOfPages).fill(null);
+  console.log(pages);
+  return (
+    <ul className={styles.pagination}>
+      {pages.map((page, index) => {
+        return (
+          <li key={index}>
+            <NavLink>{index + 1}</NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
